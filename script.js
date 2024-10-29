8b2498963bcf9dd224e4cf7a757e8d19d8b4a87a
Guilherme Rezende <guillerplr@gmail.com>
2024-10-29 16:22:32 -0300

    reconhecendo marcados e desmarcados

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const btn = document.querySelector('#rolar')
+const botao = document.querySelector('#rolar')
+botao.addEventListener('click',handleClick)
 
-btn.addEventListener('click',handleClick)
+resultados = ['⚀','⚁','⚂','⚃','⚄','⚅']
 
-dado = ['⚀','⚁','⚂','⚃','⚄','⚅']
+let rodada = 1;
 
+let dados = document.getElementsByTagName('input')
+dados = Array.from(dados)
 function rolar(){
     return Math.floor(Math.random() * 6) + 1;
 }
 
-function handleClick(){
+function handleClick(btn){
+    btn.preventDefault()
+    
+    dados.forEach(d =>{
+        if(d.checked){
+            console.log(d.id + "marcado")
+        }else{
+            console.log(d.id + "desmarcado")
+        }   
+    })
 
-   
+    
 }
