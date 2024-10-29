6d81d9c5c041e653045c4eeba6e9989d17d986ec
Guilherme Rezende <guillerplr@gmail.com>
2024-10-29 16:57:29 -0300

    Finaliza a rodada após a 3ª e desativa os botoes

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-const botao = document.querySelector('#rolar')
-botao.addEventListener('click',handleClick)
+const botao = document.querySelector("#rolar");
+botao.addEventListener("click", handleClick);
 
-resultados = ['⚀','⚁','⚂','⚃','⚄','⚅']
+const h2 = 
 
-let rodada = 1;
+resultados = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
 
-let dados = document.getElementsByTagName('input')
-dados = Array.from(dados)
-function rolar(){
-    return Math.floor(Math.random() * 6) + 1;
+let dados = document.getElementsByTagName("input");
+
+dados = Array.from(dados);
+
+let rodada = 0;
+
+const tentativas = document.querySelectorAll("#try");
+
+function rolar() {
+  return Math.floor(Math.random() * 6) + 1;
 }
 
-function handleClick(btn){
-    btn.preventDefault()
-    
-    dados.forEach(d =>{
-        if(d.checked){
-            console.log(d.id + "marcado")
-        }else{
-            console.log(d.id + "desmarcado")
-        }   
+function handleClick(btn) {
+  btn.preventDefault();
+  console.log(rodada)
+  if (rodada < 2) {
+    tentativas[rodada].classList.add("foi");
+    rodada = rodada + 1;
+  }else{
+    botao.disabled = true;
+    tentativas[rodada].classList.add("foi");
+    dados.forEach((d) =>{
+        d.checked = true;
+        d.disabled = true;
     })
+  }
 
-    
+  dados.forEach((d) => {
+    if (d.checked) {
+      console.log(d.id + "marcado");
+    } else {
+      console.log(d.id + "desmarcado");
+    }
+  });
+}
+
+function reinicia(){
+    dados.forEach((d)=>{
+        d.checked = false;
+    })
+    rodada = 0;
+    botao.disabled = false;
+
 }
