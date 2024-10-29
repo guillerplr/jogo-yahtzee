50e805f0ce4674d6a707f15ee489fb7b028d7625
Guilherme Rezende <guillerplr@gmail.com>
2024-10-29 17:42:06 -0300

    sorteio dos dados visualmente ok

--- script.js
+++ script.js
@@ -1,53 +1,64 @@
 const botao = document.querySelector("#rolar");
 botao.addEventListener("click", handleClick);
 
-const h2 = (resultados = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]);
-const labels = []
+const resultados = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
+const labels = [];
 
 let dados = document.querySelectorAll('input[type="checkbox"]');
-dados.forEach((e,i) => {
+dados.forEach((e, i) => {
   labels[i] = document.querySelector('label[for="' + e.id + '"]');
 });
+
+dados.forEach((d) => {
+    d.disabled = true;
+  });
 
 let rodada = 0;
 
 const tentativas = document.querySelectorAll("#try");
 
 function rolar() {
-  return Math.floor(Math.random() * 6) + 1;
+  return resultados[Math.floor(Math.random() * 6)];
 }
 
 function handleClick(btn) {
   btn.preventDefault();
-  console.log(rodada);
+
   if (rodada < 2) {
     tentativas[rodada].classList.add("foi");
-    rodada = rodada + 1;
-  } else {
+    dados.forEach((d) => {
+        d.disabled = false;
+      });
+ } else {
     botao.disabled = true;
     tentativas[rodada].classList.add("foi");
     dados.forEach((d) => {
-      d.checked = true;
       d.disabled = true;
     });
   }
+  rodada = rodada + 1;
 
   dados.forEach((d, i) => {
-    if (d.checked) {
-      console.log(d.id + "marcado");
-    } else {
-      let num = rolar();
-      labels[i].innerHTML = num
+    if (!d.checked) {
+      let valor = rolar();
+      labels[i].innerHTML = valor;
     }
   });
+  if(rodada == 3){
+    dados.forEach(d=>{
+        d.checked = true;
+    })
+  }
+
 }
 
 const teste = document.getElementsByTagName("h2");
 
 function reinicia() {
+    
   dados.forEach((d) => {
     d.checked = false;
-    d.disabled = false;
+    d.disabled = true;
   });
   rodada = 0;
   botao.disabled = false;
